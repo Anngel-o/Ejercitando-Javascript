16c2461b2c00c9c84268f67f585c7bd3414a3a52
Ángel Ortiz <anngelo.omz@gmail.com>
2024-12-29 02:24:43 -0600

    improve last excercise

--- 6_suma_array.js
+++ 6_suma_array.js
@@ -4,9 +4,17 @@
 
 let suma = 0;
 const array = Array.from({ length: 10 }, (_, i) => (i = i + 1));
-array.map((i) => {
+
+array.forEach((i) => {
   suma += i;
 });
+
+// const suma = array.reduce((acumulador, valorActual) => {
+//   return acumulador + valorActual;
+// }, 0);
+
+// array.reduce():Primer parámetro: Una función que toma dos argumentos: el acumulador y el valor actual. Esta función define cómo se va a acumular el resultado. En este caso, simplemente estamos sumando el acumulador y el valor actual.
+// Segundo parámetro: 0 es el valor inicial del acumulador. Si no lo proporcionamos, el acumulador tomará el primer valor del array como inicio (lo que no es adecuado para una suma).
 
 console.log(array);
 console.log(suma);
