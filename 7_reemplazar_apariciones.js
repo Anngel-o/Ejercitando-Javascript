c8959488f70f0a023c830b35837de6f581f04a2a
Ángel Ortiz <anngelo.omz@gmail.com>
2024-12-31 01:52:31 -0600

    resolve excercise with replace method strings

---
+++ 7_reemplazar_apariciones.js
@@ -0,0 +1,20 @@
+// Reemplazar todas las apariciones de un carácter en una cadena
+//         Descripción: Cambia todas las apariciones de un carácter en una cadena por otro carácter.
+//         Pistas: Usa replace() con expresiones regulares globales.
+
+function replaceCharacter(originalString, chairToChange, newChair) {
+  let result = originalString.replace(chairToChange, newChair);
+  console.log("Cadena replace: " + result);
+}
+
+let string = "Hola mundo hola mundo hola mundo";
+console.log("Cadena original: " + string);
+replaceCharacter(string, "hola", "adios");
+
+//EXPRESIONES GLOBALES
+let = newString = string.replace(/hola/gi, "adios");
+console.log("Cadena Regex: " + newString);
+
+// g: Reemplaza todas las apariciones de una subcadena(palabra o caracter) en la cadena por otra.
+// i: Insensible a mayúsculas/minúsculas.
+// m: Permite coincidencias en múltiples líneas.
